6ea5c75abc18379644556265c512c01a155733e6
marcel-zimmer <marcel-zimmer@hotmail.com>
2024-08-19 21:07:43 -0300

    rust instalado

---
+++ teste_rust/target/debug/deps/teste_rust-d06f8ef0dc283d9a.d
@@ -0,0 +1,5 @@
+/home/virus/Documents/rust/rust_exercicios/teste_rust/target/debug/deps/teste_rust-d06f8ef0dc283d9a: src/main.rs
+
+/home/virus/Documents/rust/rust_exercicios/teste_rust/target/debug/deps/teste_rust-d06f8ef0dc283d9a.d: src/main.rs
+
+src/main.rs:
---
+++ teste_rust/target/debug/teste_rust.d
@@ -0,0 +1,1 @@
+/home/virus/Documents/rust/rust_exercicios/teste_rust/target/debug/teste_rust: /home/virus/Documents/rust/rust_exercicios/teste_rust/src/main.rs
